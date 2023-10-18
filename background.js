226a5acc484711c548fd4a259aa181c0f9f1aab5
Tommy Sun <tommysun10@hotmail.com>
2023-10-18 15:32:03 -0700

    single API call

--- background.js
+++ background.js
@@ -1,59 +1,42 @@
-importScripts('config.js');
-function getCurrentTab() {
-  return new Promise((resolve, reject) => {
-    let queryOptions = { active: true, lastFocusedWindow: true };
-    chrome.tabs.query(queryOptions, function (tabs) {
-      if (tabs && tabs.length > 0) {
-        resolve(tabs[0]);
-      } else {
-        reject(new Error("No active tab found"));
-      }
-    });
-  });
-}
-
+const API_KEY = ""
 async function translationFunction(selectedLanguage, API_KEY) {
-  const style = document.createElement("style");
-  style.textContent = `
-      .translated-word {
-          background-color: yellow;
-          border-bottom: 1px dotted black;
-          color: black;
-      }
-      `;
-  document.head.append(style);
-  await translateTextNodes(document.body, selectedLanguage);
-  async function translateTextNodes(node, selectedLanguage) {
-    if (node.nodeType === Node.TEXT_NODE && node.textContent.trim() !== "") {
-      const words = node.textContent.split(/\s+/);
-      let translatedCount = 0;
-      for (let i = 0; i < words.length; i++) {
-        const word = words[i];
-        if (Math.random() < 0.02 && translatedCount < 50) {
-          const translation = await getTranslation(
-            word,
-            selectedLanguage,
-            API_KEY
-          );
-          words[
-            i
-          ] = `<span class="translated-word" title="${word}">${translation}</span>`;
-          translatedCount++;
+  const WORDS_TO_TRANSLATE = 5
+  const PROBABILITY_OF_TRANSLATION = 0.05
+
+
+  const uniqueWordsToTranslate = []
+  let uniqueTranslatedWords = []
+  const seenWords = new Set()
+  let translatedCount = 0;
+  const uniqueSplitter = "|||...|||"
+  const languageDictionary = {}
+
+  getUniqueWordsToTranslate(document.body);
+  await getTranslation(uniqueWordsToTranslate.join(uniqueSplitter), selectedLanguage)
+  createLanguageMapping()
+  replaceAllWords(document.body)
+
+  function getUniqueWordsToTranslate(node) {
+    const allWordNodes = textNodesUnder(node)
+      for (let node of allWordNodes) {
+        for (let word of node.nodeValue.split(" ")) {
+          if (Math.random() < PROBABILITY_OF_TRANSLATION && translatedCount < WORDS_TO_TRANSLATE && !seenWords.has(word.toLowerCase())) {
+            translatedCount += 1;
+            uniqueWordsToTranslate.push(word)
+            seenWords.add(word.toLowerCase())
+          }
         }
       }
+  }
 
-      const temp = document.createElement("div");
-      temp.innerHTML = words.join(" ");
+  function textNodesUnder(el) {
+    var n, a = [], walk = document.createTreeWalker(el, NodeFilter.SHOW_TEXT, null, false);
+    while (n = walk.nextNode()) a.push(n);
 
-      while (temp.firstChild) {
-        node.parentNode.insertBefore(temp.firstChild, node);
-      }
-      node.parentNode.removeChild(node);
-    } else {
-      for (const child of Array.from(node.childNodes)) {
-        await translateTextNodes(child, selectedLanguage);
-      }
-    }
+    return a.filter(n => n.nodeValue).map((n) => {
+      n.nodeValue = n.nodeValue.replace("\n", "").trim()
+      return n
+    }).filter(n => n.nodeValue)
   }
 
   async function getTranslation(text, targetLanguage) {
@@ -73,17 +56,48 @@ async function translationFunction(selectedLanguage, API_KEY) {
     });
 
     const data = await response.json();
-    return data.data.translations[0].translatedText;
+    const fullText = data.data.translations[0].translatedText;
+    uniqueTranslatedWords = fullText.replaceAll(" ", "").split(uniqueSplitter)
   }
+
+  function createLanguageMapping() {
+    for (let i = 0; i < uniqueWordsToTranslate.length; i++) {
+      languageDictionary[uniqueWordsToTranslate[i]] = uniqueTranslatedWords[i]
+    }
+  }
+
+  function replaceAllWords(node) {
+    const allWordNodes = textNodesUnder(node)
+      for (let node of allWordNodes) {
+        let newNodeValue = ""
+        for (let word of node.nodeValue.split(" ")) {
+          const wordToAdd = word in languageDictionary ? languageDictionary[word] : word
+          newNodeValue = newNodeValue + " " + wordToAdd
+        }
+        node.nodeValue = newNodeValue
+      }
+  }
+}
+
+function getCurrentTab() {
+  return new Promise((resolve, reject) => {
+    let queryOptions = { active: true, lastFocusedWindow: true };
+    chrome.tabs.query(queryOptions, function (tabs) {
+      if (tabs && tabs.length > 0) {
+        resolve(tabs[0]);
+      } else {
+        reject(new Error("No active tab found"));
+      }
+    });
+  });
 }
 
 chrome.runtime.onMessage.addListener(async (message, sender, sendResponse) => {
   if (message.action === "translatePage") {
     const tab = await getCurrentTab();
-    console.log("config: ", config);
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      args: [message.language, config.API_KEY],
+      args: [message.language, API_KEY],
       func: translationFunction,
     });
 
